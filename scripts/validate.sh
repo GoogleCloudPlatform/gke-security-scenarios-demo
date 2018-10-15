b7cc47d6869d59d614feb5ba665f7a1af7014dc7
James Donovan <jamesdonovan@google.com>
2018-10-15 10:59:09 -0500

    fixed header

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
+
 # Copyright 2018 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#k
+#
 #     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
